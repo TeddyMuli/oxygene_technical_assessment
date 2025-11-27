f22e46d4f78e3d892be7ae12efc8d945040e0049
TeddyMuli <tmuli974@gmail.com>
2025-11-27 16:01:37 +0300

    fix: add composite primary key to link table

--- backend/app/models/bookmarktag.py
+++ backend/app/models/bookmarktag.py
@@ -2,5 +2,5 @@ from sqlmodel import SQLModel, Field
 import uuid
 
 class BookmarkTag(SQLModel, table=True):
-    bookmark_id: uuid.UUID = Field(foreign_key="bookmark.id")
-    tag_id: uuid.UUID = Field(foreign_key="tag.id")
+    bookmark_id: uuid.UUID = Field(foreign_key="bookmark.id", primary_key=True)
+    tag_id: uuid.UUID = Field(foreign_key="tag.id", primary_key=True)
