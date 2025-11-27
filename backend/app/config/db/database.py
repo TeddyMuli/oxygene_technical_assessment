f9129afe2b5d012bae363d45bcb1008c8c3b3a92
TeddyMuli <tmuli974@gmail.com>
2025-11-27 11:49:26 +0300

    update database config

--- backend/app/config/db/database.py
+++ backend/app/config/db/database.py
@@ -2,12 +2,13 @@ from typing import Annotated
 
 from sqlmodel import Session, create_engine, SQLModel
 from app.config.config import settings
+from fastapi import Depends
 
 database_url = f"{settings.DATABASE_TYPE}://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}/{settings.DATABASE_NAME}?sslmode=require"
 
 engine = create_engine(database_url)
 
-def create_db_and_tables():
+def init_db():
     SQLModel.metadata.create_all(engine)
 
 def get_session():
