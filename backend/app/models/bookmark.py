3efea933360a9104fbf54c3c9d2f0ae6d31f896e
TeddyMuli <tmuli974@gmail.com>
2025-11-27 12:34:21 +0300

    define one to many relationship between user and bookmark model

--- backend/app/models/bookmark.py
+++ backend/app/models/bookmark.py
@@ -1,5 +1,6 @@
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 from app.models.basemodel import BaseModel
+from app.models.user import User
 import uuid
 
 class BookMark(BaseModel):
@@ -7,3 +8,5 @@ class BookMark(BaseModel):
     user_id: uuid.UUID  = Field(foreign_key="user.id")
     url: str
     description: str
+
+    user: list["User"] = Relationship(back_populates="bookmarks")
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,10 @@
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 from app.models.basemodel import BaseModel
+from app.models.bookmark import BookMark
 
 class User(BaseModel):
     email: str = Field(unique=True, index=True)
     name: str
     password: str
+
+    bookmarks: list["BookMark"] = Relationship(back_populates="user", cascade_delete=True)
