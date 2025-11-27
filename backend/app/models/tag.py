e3cb171fb0ab6afaad524b5c6f7bbaf1ce3eb5ac
TeddyMuli <tmuli974@gmail.com>
2025-11-27 15:55:03 +0300

    fix relationship in tag model

--- backend/app/models/tag.py
+++ backend/app/models/tag.py
@@ -1,9 +1,9 @@
 from app.models.basemodel import BaseModel
 from app.models.bookmarktag import BookmarkTag
-from app.models.tag import Tag
+from app.models.bookmark import BookMark
 from sqlmodel import Field, Relationship
 
 class Tag(BaseModel):
     name: str = Field(unique=True, index=True)
 
-    bookmarks: list["Tag"] = Relationship(back_populates="tags", link_model=BookmarkTag)
+    bookmarks: list["BookMark"] = Relationship(back_populates="tags", link_model=BookmarkTag)
