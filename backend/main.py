3eae21fcfc2ee625811b08b16f3df0b10741bb13
TeddyMuli <tmuli974@gmail.com>
2025-11-28 22:05:03 +0300

    fix cors error

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.config.db.database import init_db
 from app.config.config import settings
 from app.routes.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +13,19 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "https://oxygene-bookmarks.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
